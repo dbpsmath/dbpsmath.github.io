e020754b3386a71b22b04cfa46e530acff47d749
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-10-21 01:08:54 +0000

    yes

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,7 +1,7 @@
-localStorage.setItem('theme', 'modern')
-
-
-if (localStorage.getItem("theme")) {
-    document.body.setAttribute("theme", localStorage.getItem("theme"))
-}
-console.log(localStorage.getItem("theme"))
+document.addEventListener("DOMContentLoaded", function() {
+    const quoteText = document.querySelector(".motd")
+    var motds = Array("Please wait while your MOTD is loading...", "Bone appetit!", "iShowSpeed is cringe", "I love javascript (please send help)", "Made using github codespaces", "Not google sites!", '"Easy enough for a toddler, not easy for a sys admin"', '"Goodbye 3kh0 ):"', '"Schools are literally 1984"', 'Imagine not blocking this', 'This is a random MOTD', '"Be responsible" -No one', '"I cant think of any more" -DBP', '"Please contribute MOTDs" - DBP', '"How do I type?"', "The closest we've ever gotten to world peace is when everyone did the gagnam style", "Approved by me (unofficial)", "I can't think of anything", "Support Ukraine!", "Here's a math fact to recoop for your time there spent playing games, you'll never need it in life", "hyperplexed do the hub", "Thin crust", "Fun fact: The song world cup isn't, and will never be funny.", "skid");
+  
+    var randomMOTD = motds[Math.floor(Math.random() * motds.length)];
+    quoteText.innerHTML = randomMOTD
+});
