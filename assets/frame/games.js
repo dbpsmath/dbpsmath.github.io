1f7056c9fb37a37a0030ece103ded475c66e16aa
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-10-22 15:35:37 +0000

    yea wooo

--- assets/frame/games.js
+++ assets/frame/games.js
@@ -81,6 +81,69 @@ var games = [
       description: 'Duck Life 3 is a funny running game. Your mission is to control a duckling to run, swim and fly to reach the finish line to earn as many coins as possible.',
       url: 'https://' + document.location.host + '/' + 'assets/gxmes/ducklife3/',
       image: 'assets/gxmes/ducklife3/duck.png'
+    },
+    {
+      id: 'smashkarts',
+      title: 'Smash Karts',
+      description: 'Smash Karts is a free io Multiplayer Kart Battle Arena game. Drive fast. Fire rockets. Make big explosions.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/smashkarts/',
+      image: 'assets/gxmes/smashkarts/gameicon.png'
+    },
+    {
+      id: 'subway',
+      title: 'Subway Surfers',
+      description: 'Subway Surfers is a classic endless runner game. You play as Jake, who surfs the subways and tries to escape from the grumpy Inspector and his dog. Youll need to dodge trains, trams, obstacles, and more to go as far as you can in this endless running game. Collect coins to unlock power-ups and special gear',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/subway-surfers/',
+      image: 'assets/gxmes/subway-surfers/gameicon.png'
+    },
+    {
+      id: 'doge',
+      title: 'Doge Miner',
+      description: 'Mines into the future where infinite dogecoins exist. Buy! How to mine dogecoins? Find out in Dogeminer: The Dogecoin Mining Simulator. Very wow. To the moon!',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/dogeminer/',
+      image: 'assets/gxmes/dogeminer/gameicon.png'
+    },
+    {
+      id: 'doge2',
+      title: 'Doge Miner 2',
+      description: 'A space-travel, rock-crusher, free clicker adventure game. Build such mining operation, get much dogecoins. Hire many help to increase dogecoin per second, improve stats like +wow by finding loot and more. Buy Space Rockets to reach the moon and beyond. Play here.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/dogeminer2/',
+      image: 'assets/gxmes/dogeminer2/gameicon.png'
+    },
+    {
+      id: 'flappy',
+      title: 'Flappy Bird',
+      description: 'Flappy Bird is a mobile game developed by the Vietnamese video game artist and programmer Dong Nguyen, under his game development company.Gears. The game is a side-scroller where the player controls a bird, attempting to fly between columns of green pipes without hitting them.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/flappybird/',
+      image: 'assets/gxmes/flappybird/flappy.png'
+    },
+    {
+      id: 'monkeymart',
+      title: 'MonkeyMart',
+      description: 'Monkey Mart is an idle/management game where you control a cute monkey character who is in charge of a supermarket. Plant fruits, harvest produce, move around from station to station to fill the stands with various food items. Sell bananas, corn, eggs, peanuts, coffee beans, chocolate, wheat etc..',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/monkeymart/',
+      image: 'assets/gxmes/monkeymart/gameicon.png'
+    },
+    {
+      id: 'ovo',
+      title: 'OvO',
+      description: 'OvO.io Online is the same as the OvO game you know and love, but now in multiplayer! This is a fast-paced skill-based platforming game focused on maneuvering around the levels as fast as possible. Complete every level available in easy and hard mode.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ovo/',
+      image: 'assets/gxmes/ovo/gameicon.png'
+    },
+    {
+      id: 'duckspace',
+      title: 'Duck Life Space',
+      description: 'Duck Life: Space is a duck racing game with a galactic story and theme. An alien from outer space has nabbed your trophy, and you need to go on a quest through space to get it back! Enjoy a plethora of new mini-games and races along the way.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ducklifespace/duck5/',
+      image: 'assets/gxmes/ducklifespace/gameicon.png'
+    },
+    {
+      id: 'ovo2',
+      title: 'OvO 2',
+      description: 'OvO 2 is an exciting and immersive video game that takes players on a thrilling journey through a captivating virtual world. This sequel to the original OvO',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ovo2/',
+      image: 'assets/gxmes/ovo2/gameicon.png'
     }
   ]
   var customgames = getObj("customgames")
