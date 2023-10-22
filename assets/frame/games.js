3919c04624960effc639e4a081ed324681b50a04
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-10-22 00:12:57 +0000

    last for the night

--- assets/frame/games.js
+++ assets/frame/games.js
@@ -46,6 +46,41 @@ var games = [
       description: 'Can you pass the grade and take your team all the way to the ultimate prize? Can you win the Retro Bowl?',
       url: 'https://' + document.location.host + '/' + 'assets/gxmes/retrobowl/',
       image: 'assets/gxmes/retrobowl/img/icon.jpg'
+    },
+    {
+      id: 'eggy',
+      title: 'Eggy Car',
+      description: 'Eggy is not wearing a seatbelt! Try your best not to scramble him!',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/eggycar/',
+      image: 'assets/gxmes/eggycar/gameicon.png'
+    },
+    {
+      id: 'snowrider',
+      title: 'Snow Rider 3D',
+      description: 'Snow Rider 3D is a .io game. Snow Rider 3D is designed based on the idea to bring Christmas all year round. This game gives you an opportunity to slide down a giant slope',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/snowrider3d/',
+      image: 'assets/gxmes/snowrider3d/gameicon.png'
+    },
+    {
+      id: 'ducklife1',
+      title: 'Duck Life 1',
+      description: 'Duck Life 1 is a funny running game. Your mission is to control a duckling to run, swim and fly to reach the finish line to earn as many coins as possible.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ducklife1/',
+      image: 'assets/gxmes/ducklife1/ducklife.png'
+    },
+    {
+      id: 'ducklife2',
+      title: 'Duck Life 2',
+      description: 'Duck Life 2 is a funny running game. Your mission is to control a duckling to run, swim and fly to reach the finish line to earn as many coins as possible.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ducklife2/',
+      image: 'assets/gxmes/ducklife2/ducklife2.png'
+    },
+    {
+      id: 'ducklife3',
+      title: 'Duck Life 3: Evolution',
+      description: 'Duck Life 3 is a funny running game. Your mission is to control a duckling to run, swim and fly to reach the finish line to earn as many coins as possible.',
+      url: 'https://' + document.location.host + '/' + 'assets/gxmes/ducklife3/',
+      image: 'assets/gxmes/ducklife3/duck.png'
     }
   ]
   var customgames = getObj("customgames")
---
+++ assets/gxmes/snowrider3d/README.md
@@ -0,0 +1,2 @@
+# snowrider3d
+Use the arrow keys or `A` and `D` to move. Use `W` or the up arrow to jump. Avoid obstacles by dodging them or going over them.
