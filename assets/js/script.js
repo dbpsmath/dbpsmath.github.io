7098d6d3ce5ae5c05f4cacc0b807659f1c7dc3dc
DBP <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-11-11 08:10:22 -0600

    MOTDs and better script injection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const quoteText = document.querySelector(".motd")
-    var motds = Array("I still remember v1 😭", "It is forbidden to download/upload fish", "Please wait while your MOTD is loading...", "Another MOTD", "Bone appetit!", "iShowSpeed is cringe", "I love javascript (please send help)", "Made using github codespaces", "Not google sites!", '"Easy enough for a toddler, not easy for a sys admin"', '"Goodbye 3kh0 ):"', '"Schools are literally 1984"', 'Imagine not blocking this', 'This is a random MOTD', '"Be responsible" -No one', '"I cant think of any more" -DBP', '"Please contribute MOTDs" - DBP', '"How do I type?"', "The closest we've ever gotten to world peace is when everyone did the gagnam style", "Approved by me (unofficial)", "I can't think of anything", "Support Ukraine!", "Here's a math fact to recoop for your time there spent playing games, you'll never need it in life", "hyperplexed do the hub", "Thin crust", "Fun fact: The song world cup isn't, and will never be funny.", "skid");
+    var motds = Array("You got the ultra rare (Loading...)", "DONT ASK ABOUT MY DISCORD PFP AND USERNAME", "DONT ASK ABOUT MY DISCORD PFP AND USERNAME", "DONT ASK ABOUT MY DISCORD PFP AND USERNAME", "DONT ASK ABOUT MY DISCORD PFP AND USERNAME", "I still remember v1 😭", "It is forbidden to download/upload fish", "Please wait while your MOTD is loading...", "Another MOTD", "Bone appetit!", "iShowSpeed is cringe", "I love javascript (please send help)", "Made using github codespaces", "Not google sites!", '"Easy enough for a toddler, not easy for a sys admin"', '"Goodbye 3kh0 ):"', '"Schools are literally 1984"', 'Imagine not blocking this', 'This is a random MOTD', '"Be responsible" -No one', '"I cant think of any more" -DBP', '"Please contribute MOTDs" - DBP', '"How do I type?"', "The closest we've ever gotten to world peace is when everyone did the gagnam style", "Approved by me (unofficial)", "I can't think of anything", "Support Ukraine!", "Here's a math fact to recoop for your time there spent playing games, you'll never need it in life", "hyperplexed do the hub", "Thin crust", "Fun fact: The song world cup isn't, and will never be funny.", "skid");
   
     var randomMOTD = motds[Math.floor(Math.random() * motds.length)];
     quoteText.innerHTML = randomMOTD
@@ -19,20 +19,15 @@ function abcloak() {
     tab.document.body.appendChild(iframe);
 }
 
-// Create the script element
 var scriptElement = document.createElement('script');
 
-// Set the source and async attributes
 scriptElement.src = 'https://www.googletagmanager.com/gtag/js?id=G-5R6BKQ11BM';
 scriptElement.async = true;
 
-// Append the script to the head of the document
 document.head.appendChild(scriptElement);
 
-// Create the inline script
 var inlineScript = document.createElement('script');
 
-// Set the script content
 inlineScript.innerHTML = `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
