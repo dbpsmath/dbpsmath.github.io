2e0a8ccadd157430e7ffd694878b8153d77b8619
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-11-12 14:24:33 -0600

    Update load.js

--- assets/frame/load.js
+++ assets/frame/load.js
@@ -6,7 +6,7 @@ if (params.get("game")) {
     document.querySelector("#gameImage").src = game.image
     document.querySelector("#gameTitle").innerHTML = game.title
     if (game.description) document.querySelector("#gameDescription").innerHTML = game.description
-    document.querySelector("#frame").src = game.url    
+    document.querySelector("#frame").src = game.url
     })
 }
 
